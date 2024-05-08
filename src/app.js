004d4e03656065117eed2f44c44935fff53e3fca
Cristinacbm5 <157827345+Cristinacbm5@users.noreply.github.com>
2024-05-08 06:09:34 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronombres = ["mi", "tu", "su"];
+  const adjetivos = ["gran", "pequeño", "rápido", "lento"];
+  const sustantivos = ["empresa", "negocio", "sitio", "web"];
+
+  const combinaciones = [];
+
+  for (const pronombre of pronombres) {
+    for (const adjetivo of adjetivos) {
+      for (const sustantivo of sustantivos) {
+        const dominio = pronombre + adjetivo + sustantivo + ".com";
+        combinaciones.push(dominio);
+      }
+    }
+  }
+
+  for (const dominio of combinaciones) {
+    console.log(dominio);
+  }
 };
